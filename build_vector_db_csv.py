9e00e47ea00982eb1dca28b3da4028d9b5fd17a0
Dufour <quentin.dufour@epfprojets.com>
2025-05-21 13:44:36 +0200

    features: rag model v2

---
+++ build_vector_db_csv.py
@@ -0,0 +1,46 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+from langchain_community.document_loaders import PyPDFLoader 
+import uuid
+
+dataframe=pd.read_csv("data.csv")
+embeddings=OllamaEmbeddings(model="mxbai-embed-large")
+
+db_location="./chrome_langchain_db"
+
+vector_store=Chroma(
+    collection_name="legal_rules",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+
+
+
+documents=[]
+ids=[]
+
+for i, row in dataframe.iterrows():
+    document=Document(
+        page_content=row["Thématique"] + " " + row["Réglementation"],
+        metadata={
+            "obligatoire": row["Obligatoire"],
+            "source": row["Source"]
+        },
+        id=str(i)
+    )
+    ids.append(str(i))
+    documents.append(document)
+    
+
+
+vector_store.add_documents(documents=documents, ids=ids)
+
+retriever=vector_store.as_retriever(
+    search_kwargs={
+        "k":5
+        }
+)
--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
 
-model = OllamaLLM(model="llama3.2")
+
+
+model = OllamaLLM(model="llama3.2:latest")
+#model=OllamaLLM(model="llama2:7b")
 
 template = """
-You are a legal assistant specialized in Junior-Enterprises.
+You're the ebst chemistry teacher ever.
 
-You must answer the following question **only** using the provided reviews. 
-If the answer is not in the reviews, say: "Je ne trouve pas cette information dans les documents fournis."
+You must answer the following question (in french) **only** using the provided reviews. 
+If the answer is not in the reviews, say: "I cannot find this document in the document provided."
 
-Here are the reviews:
+Here are the reviews (you have to give the pages where you find information):
 {reviews}
 
 Question:
